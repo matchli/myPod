77531a1bef2fc2ab801a4d961666eecd681e1bca
matchli <459044374@qq.com>
2018-03-01 09:54:49 +0800

    创建私有Pod

---
+++ myPod/myPod/myPod.h
@@ -0,0 +1,19 @@
+//
+//  myPod.h
+//  myPod
+//
+//  Created by lq on 2018/3/1.
+//  Copyright © 2018年 lq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for myPod.
+FOUNDATION_EXPORT double myPodVersionNumber;
+
+//! Project version string for myPod.
+FOUNDATION_EXPORT const unsigned char myPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myPod/PublicHeader.h>
+
+
